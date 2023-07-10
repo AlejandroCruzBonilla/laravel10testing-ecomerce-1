553626e4f40762a5e1d0fd5b18eef69d0434db2b
Alejandro Cruz Bonilla <alex.139139.cb@gmail.com>
2023-07-10 17:02:53 -0600

    install Breeze blade

--- src/ecomerce/src/resources/js/app.js
+++ src/ecomerce/src/resources/js/app.js
@@ -1,4 +1,12 @@
 import './bootstrap';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+
 import { createApp } from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
 import ExampleComponent from "./components/ExampleComponent.vue";
