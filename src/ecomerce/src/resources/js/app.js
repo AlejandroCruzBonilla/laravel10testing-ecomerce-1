9e38918af1bfaf84c4b78a833ac5ad75a30f2b0c
Alejandro Cruz Bonilla <alex.139139.cb@gmail.com>
2023-07-10 16:47:32 -0600

    install Vue

--- src/ecomerce/src/resources/js/app.js
+++ src/ecomerce/src/resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+import { createApp } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import ExampleComponent from "./components/ExampleComponent.vue";
+
+const app = createApp({
+    components: {
+        HelloWorld,
+        ExampleComponent
+    },
+});
+
+app.mount("#app");
