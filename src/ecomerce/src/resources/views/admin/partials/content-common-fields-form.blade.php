31353224bf2472d6cd32b0708140509923f92465
Alejandro Cruz Bonilla <alex.139139.cb@gmail.com>
2023-07-22 11:07:31 -0600

    fix

--- src/ecomerce/src/resources/views/admin/partials/content-common-fields-form.blade.php
+++ src/ecomerce/src/resources/views/admin/partials/content-common-fields-form.blade.php
@@ -7,11 +7,11 @@
 
 <div class="input-group mb-3 input_box">
     <label for="status" class="inline-flex items-center">
+        <input type="hidden" name="status" value="unpublished">
         <input id="status" type="checkbox" name="status" value="published"
             {{ old('status', $node->status) === 'published' ? 'checked' : '' }}
             class="rounded border-solid border-gray-900 text-indigo-600 shadow-sm focus:ring-indigo-500">
         <span class="ml-2 text-sm text-gray-600">{{ __('Published') }}</span>
     </label>
-    <input type="hidden" name="status" value="unpublished">
     <x-forms.input-error class="mt-2" :messages="$errors->get('status')" />
 </div>
