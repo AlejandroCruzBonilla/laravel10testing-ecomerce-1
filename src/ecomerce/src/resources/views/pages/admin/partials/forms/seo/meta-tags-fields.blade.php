72e80e1c60a4f96f259a57f82e4b65dd7538f17a
Cruz Bonilla Alejandro <alex.139139.cb@gmail.com>
2023-08-10 16:39:55 -0600

    .

--- src/ecomerce/src/resources/views/pages/admin/partials/forms/seo/meta-tags-fields.blade.php
+++ src/ecomerce/src/resources/views/pages/admin/partials/forms/seo/meta-tags-fields.blade.php
@@ -8,7 +8,7 @@
 		:value="old('seo[meta-title]', '')"
 		autofocus
 		autocomplete="seo[meta-title]" />
-    <x-forms.input-error class="mt-2" :messages="$errors->get('seo[meta-description]')" />
+    <x-forms.input-error class="mt-2" :messages="$errors->get('seo[meta-title]')" />
 </div>
 
 <div class="input-group mb-3 input_box">
