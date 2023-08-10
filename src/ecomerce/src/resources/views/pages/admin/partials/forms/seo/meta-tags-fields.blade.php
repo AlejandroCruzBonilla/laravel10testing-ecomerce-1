8b021bcdbadb0d4af322eea7d3ddb06d87f142cd
Cruz Bonilla Alejandro <alex.139139.cb@gmail.com>
2023-08-10 16:34:46 -0600

    .

--- src/ecomerce/src/resources/views/pages/admin/partials/forms/seo/meta-tags-fields.blade.php
+++ src/ecomerce/src/resources/views/pages/admin/partials/forms/seo/meta-tags-fields.blade.php
@@ -1,25 +1,25 @@
 <div class="input-group mb-3 input_box">
     <x-forms.input-label for="seo[meta-title]" :value="__('Title')" />
     <x-forms.text-input
-      id="meta-title"
-      name="seo[meta-title]"
-      type="text"
-      class="block w-full"
-      :value="old('seo[meta-title]', '')"
-      autofocus
-      autocomplete="seo[meta-title]" />
+		id="meta-title"
+		name="seo[meta-title]"
+		type="text"
+		class="block w-full"
+		:value="old('seo[meta-title]', '')"
+		autofocus
+		autocomplete="seo[meta-title]" />
     <x-forms.input-error class="mt-2" :messages="$errors->get('slug')" />
 </div>
 
 <div class="input-group mb-3 input_box">
     <x-forms.input-label for="seo[meta-description]" :value="__('Description')" />
     <x-forms.text-input
-      id="meta-description"
-      name="seo[meta-description]"
-      type="text"
-      class="block w-full"
-      :value="old('seo[meta-description]', '')"
-      autofocus
-      autocomplete="seo[meta-description]" />
+		id="meta-description"
+		name="seo[meta-description]"
+		type="text"
+		class="block w-full"
+		:value="old('seo[meta-description]', '')"
+		autofocus
+		autocomplete="seo[meta-description]" />
     <x-forms.input-error class="mt-2" :messages="$errors->get('slug')" />
 </div>
